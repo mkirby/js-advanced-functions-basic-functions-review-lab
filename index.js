72f53f7f35cd59624b5c0bea9bbb5ce552c14e8b
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-06 13:36:45 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+  return function(something) {
+    return `You are ${flair}${something}${flair}!`
+  }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a + b
+  },
+  subtract: function(a, b) {
+    return a - b
+  },
+  multiply: function(a, b) {
+    return a * b
+  },
+  divide: function(a, b) {
+    return a / b
+  }
+}
+
+function actionApplyer(int, arrayOfFunctions ) {
+  arrayOfFunctions.forEach(f => int = f(int))
+  return int
+}
